98b5368fdfc3845ad73f3468fad0dd7685f814d7
RADIOdemon6-alt <abyzaryws@gmail.com>
2025-08-04 15:00:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
 
-function switchCard(cardId) {
-    document.querySelectorAll('.form-card').forEach(card => {
-        card.classList.remove('active');
-        card.classList.add('hidden');
-    });
-    const targetCard = document.getElementById(cardId);
-    targetCard.classList.remove('hidden');
-    targetCard.classList.add('active');
-    targetCard.classList.add('glitch');
-    setTimeout(() => {
-        targetCard.classList.remove('glitch');
-    }, 300);
-}
