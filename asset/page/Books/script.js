2b46d152973376ce4f702ea9d91466eae465f719
RADIOdemon6-alt <abyzaryws@gmail.com>
2025-08-09 15:48:48 +0300

    Update script.js

--- asset/page/Books/script.js
+++ asset/page/Books/script.js
@@ -126,3 +126,11 @@ window.addEventListener("click", e => {
     popup.classList.add("hidden");
   }
 });
+const list = document.querySelectorAll('.list');
+function activeLink() {
+    list.forEach((item) =>
+    item.classList.remove('active'));
+    this.classList.add('active');
+}
+list.forEach((item) =>
+item.addEventListener('click',activeLink));
